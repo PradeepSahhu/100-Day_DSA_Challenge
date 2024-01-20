d6c48e6c77150e449ba718a74601898123c1606c
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-01-20 17:27:52 +0530

    Simple syntax of the longest_sum_subarray with complexity of O(n^3)

--- longest_sum.cpp
+++ longest_sum.cpp
@@ -17,6 +17,7 @@ vector<int> longest_sum(vector<int> &v, int k)
 
         for (int j = i + 1; j < v.size(); j++)
         {
+            // pushing the elements in to the subarray.
             for (int k = i; k <= j; k++)
             {
                 arr.push_back(v[k]);
@@ -41,19 +42,49 @@ vector<int> longest_sum(vector<int> &v, int k)
 
     int sum = 0;
 
+    // then i am calculating the sum of the subarray.
+
     for (auto it : allv)
     {
-        cout << " this is first loop" << endl;
+        // cout << " this is first loop" << endl;
         for (int i = 0; i < it.size(); i++)
         {
-            cout << it[i] << " this is second loop" << endl;
-            sum += it[i];
+            // cout << it[i] << " this is second loop" << endl;
+            sum += it[i]; // here.
         }
         if (sum == k)
         {
             return it;
         }
         sum = 0;
+    }
+}
+
+vector<int> longest_sum_subarray(vector<int> &v, int k)
+{
+
+    int sum = 0;
+    vector<int> ans;
+
+    // better syntax and easy to understand.
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = i + 1; j < v.size(); j++)
+        {
+            for (int k = i; k < j; k++)
+            {
+                ans.push_back(v[k]);
+                sum += v[k];
+            }
+
+            if (sum == k)
+            {
+                return ans;
+            }
+            sum = 0;
+            ans.clear();
+        }
     }
 }
 int main()
@@ -63,7 +94,7 @@ int main()
     int size = sizeof(arr) / sizeof(arr[0]);
 
     vector<int> v(arr, arr + size);
-    vector<int> ans = longest_sum(v, 10);
+    vector<int> ans = longest_sum_subarray(v, 10);
     for (int i = 0; i < ans.size(); i++)
     {
         cout << ans[i] << " ";
