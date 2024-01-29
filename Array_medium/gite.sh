f97c5f281d98e9c830af72bb01060c2479900ade
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-01-29 20:04:53 +0530

    rotate_vector_by_90_degree

--- Array_medium/gite.sh
+++ Array_medium/gite.sh
@@ -1,6 +1,6 @@
 #!/opt/homebrew/bin/bash
 
-if [ $#0 -eq 0 ]
+if [ $#1 -eq 0 ]
 then
 	
 	echo "Please Enter any message"
@@ -11,14 +11,16 @@ else
 	git add .
 	wait
 	echo "Added to Staging Area"
-	git commit -m "$0"
+	git commit -m "$1"
 	sleep 1
-	echo "Commited with the following message : $0"
+	echo "Commited with the following message : $1"
 	git push -u origin main --force
 	sleep 1
 	echo "Pused forcefully to the github"
 	
 fi
+
+echo "The message was : $1"
 	
 
 
