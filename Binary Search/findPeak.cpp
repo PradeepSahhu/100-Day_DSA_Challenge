7d7e11fd33c9e84fe032eee49f57958105ec0959
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-03-03 10:40:30 +0530

    solved finding the peak element

--- Binary Search/findPeak.cpp
+++ Binary Search/findPeak.cpp
@@ -53,10 +53,45 @@ int findPeakElement(vector<int> &arr)
     return -1;
 }
 
+//! self code.
+
+int findPeaksElement(vector<int> &nums)
+{
+    int n = nums.size();
+    if (n == 1 || nums[0] > nums[1])
+        return nums[0];
+    if (nums[n - 1] > nums[n - 2])
+        return nums[n - 1];
+
+    int left = 1;
+    int right = n - 2;
+
+    while (left <= right)
+    {
+        int mid = (left + right) / 2;
+
+        if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1])
+        {
+            return nums[mid];
+        }
+        else if (nums[mid] < nums[mid + 1])
+        {
+            // left side.
+            left = mid + 1;
+        }
+        else
+        {
+            // right side.
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
     vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 5, 1};
-    cout << findingPeakOfArray(nums);
+    cout << findPeaksElement(nums);
 
     //   Compilation Time code //
     cout << endl;
