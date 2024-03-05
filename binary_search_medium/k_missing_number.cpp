8e213f2a8aa956a38c83202cebad975d98d64f47
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-03-05 20:01:43 +0530

    find the k missing number

--- binary_search_medium/k_missing_number.cpp
+++ binary_search_medium/k_missing_number.cpp
@@ -8,6 +8,25 @@ auto start = chrono::steady_clock::now();
 int findKthPositive(vector<int> &arr, int k)
 {
     // find the number which is missing at kth position.
+    int n = arr.size();
+    int left = 0;
+    int right = n - 1;
+
+    while (left <= right)
+    {
+        
+        int mid = (left + right) / 2;
+        int missing = arr[mid] - (mid + 1);
+        if (missing < k)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return k + left;
 }
 int main()
 {
