0b8077c0d5c31e1108dd428f90bb704c83b8b5cf
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-03-05 19:12:30 +0530

    find the k missing number

---
+++ binary_search_medium/k_missing_number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <chrono>
+using namespace std;
+
+auto start = chrono::steady_clock::now();
+
+int findKthPositive(vector<int> &arr, int k)
+{
+
+    return 9;
+}
+int main()
+{
+
+    //   Compilation Time code //
+    cout << endl;
+    auto end = chrono::steady_clock::now();
+    auto diff = end - start;
+    cout << chrono::duration<double, milli>(diff).count() << " ms" << endl;
+    return 0;
+}
