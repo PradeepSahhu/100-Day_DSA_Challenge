4cda729675b99f9d12e6eac26903ca514d6c5609
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-03-22 23:38:05 +0530

    solved the problem

---
+++ Strings/easyStrings/lowerBound.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <chrono>
+using namespace std;
+
+auto start = chrono::steady_clock::now();
+
+int lowerBound(vector<int> arr, int n, int x)
+{
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] >= x)
+        {
+            ans = mid;
+            // look for smaller index on the left
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+int rowWithMax1s(vector<vector<int>> &matrix, int n, int m)
+{
+    int cnt_max = 0;
+    int index = -1;
+
+    // traverse the rows:
+    for (int i = 0; i < n; i++)
+    {
+        // get the number of 1's:
+        int cnt_ones = m - lowerBound(matrix[i], m, 1);
+        if (cnt_ones > cnt_max)
+        {
+            cnt_max = cnt_ones;
+            index = i;
+        }
+    }
+    return index;
+}
+int main()
+{
+
+    //   Compilation Time code //
+    cout << endl;
+    auto end = chrono::steady_clock::now();
+    auto diff = end - start;
+    cout << chrono::duration<double, milli>(diff).count() << " ms" << endl;
+    return 0;
+}
