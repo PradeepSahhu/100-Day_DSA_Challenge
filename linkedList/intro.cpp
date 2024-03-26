6f81b485ce7d75499db29dc27bd85ba23120e710
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-03-26 21:59:13 +0530

    linkedlist problem

--- linkedList/intro.cpp
+++ linkedList/intro.cpp
@@ -5,18 +5,59 @@ using namespace std;
 
 auto start = chrono::steady_clock::now();
 
-class Node
+// class Node
+// {
+// public:
+//     int data;   // Data stored in the node
+//     Node *next; // Pointer to the next node in the linked list
+//     // Constructors
+//     Node(int data1, Node *next1)
+//     {
+//         data = data1;
+//         next = next1;
+//     }
+//     // Constructor
+//     Node(int data1)
+//     {
+//         data = data1;
+//         next = nullptr;
+//     }
+// };
+
+// Node *constructLL(vector<int> &arr)
+// {
+//     Node *head = NULL;
+//     while (arr.size() != 0)
+//     {
+//         int val = arr.back();
+//         Node *temp = new Node(val);
+//         if (head == NULL)
+//         {
+
+//             head = temp;
+//         }
+//         else
+//         {
+//             temp->next = head;
+//             head = temp;
+//         }
+//         arr.pop_back();
+//     }
+
+//     return head;
+// }
+
+//! Structure Node.
+struct Node
 {
-public:
-    int data;   // Data stored in the node
-    Node *next; // Pointer to the next node in the linked list
-    // Constructors
+    int data;
+    Node *next;
+
     Node(int data1, Node *next1)
     {
         data = data1;
         next = next1;
     }
-    // Constructor
     Node(int data1)
     {
         data = data1;
@@ -24,30 +65,35 @@ public:
     }
 };
 
-Node *constructLL(vector<int> &arr)
+class Node
 {
-    Node *head = NULL;
-    while (arr.size() != 0)
+    int data;
+    Node *next;
+
+    Node(int data1, Node *next1)
     {
-        int val = arr.back();
-        Node *temp = new Node(val);
-        if (head == NULL)
-        {
-
-            head = temp;
-        }
-        else
-        {
-            temp->next = head;
-            head = temp;
-        }
-        arr.pop_back();
+        data = data1;
+        next = next1;
     }
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
 
-    return head;
-}
 int main()
 {
+    vector<int> v = {1, 2, 3, 4};
+    Node x = Node(v[0], nullptr);
+    Node *y = &x;
+    Node *z = new Node(v[1], nullptr); // new keyword is used to give pointer to the memory address.
+
+    cout << z << endl;
+    cout << &x << endl;
+    cout << y << endl;
+    cout << x.data << endl;
+    cout << z->data << endl;
 
     //   Compilation Time code //
     cout << endl;
