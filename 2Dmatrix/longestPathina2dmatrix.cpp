3988aa78e7260d1915a72ddce362a6b1bb4710e1
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-03-27 21:57:02 +0530

    Solved the longestPathInA2Dmatrix

--- 2Dmatrix/longestPathina2dmatrix.cpp
+++ 2Dmatrix/longestPathina2dmatrix.cpp
@@ -4,6 +4,51 @@
 using namespace std;
 
 auto start = chrono::steady_clock::now();
+
+vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+int longestIncreasingPath(vector<vector<int>> &matrix, vector<vector<int>> &cache, int n, int m, int i, int j)
+{
+    if (cache[i][j] > 0)
+        return cache[i][j];
+
+    int maxi = 0;
+    for (vector<int> direction : directions)
+    {
+        int x = direction[0] + i;
+        int y = direction[1] + j;
+
+        if (x > -1 && y > -1 && x < n && y < m && matrix[x][y] > matrix[i][j])
+        {
+            int longest = longestIncreasingPath(matrix, cache, n, m, x, y);
+            maxi = max(maxi, longest);
+        }
+    }
+    cache[i][j] = maxi + 1;
+    return cache[i][j];
+}
+
+int longestIncreasingPath(vector<vector<int>> &mat, int n, int m)
+{
+    if (mat.size() == 0)
+    {
+        return 0;
+    }
+
+    int longestPath = 0;
+    vector<vector<int>> cache(n, vector<int>(m)); // n - rows and m columns.
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int longest = longestIncreasingPath(mat, cache, n, m, i, j);
+            longestPath = max(longestPath, longest);
+        }
+    }
+    return longestPath;
+}
+
 int main()
 {
 
