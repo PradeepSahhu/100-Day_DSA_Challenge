13c8ed6292deab0d646058a2235a78fb7e94be13
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-04-03 23:57:18 +0530

    Solved the quesitons

--- medium_linkedList/sort_linked_listhavingzero'sandOne's.cpp
+++ medium_linkedList/sort_linked_listhavingzero'sandOne's.cpp
@@ -8,48 +8,48 @@ auto start = chrono::steady_clock::now();
 class Node
 {
 public:
-int data;
-Node *next;
-Node(int data, Node *next)
-{
-this->data = data;
-this->next = next;
-}
-
-Node(int data)
-{
-this->data = data;
-this->next = nullptr;
+    int data;
+    Node *next;
+    Node(int data, Node *next)
+    {
+        this->data = data;
+        this->next = next;
     }
-    };
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
 void insert_in_tail(Node *&head, int element)
 {
-Node *newNode = new Node(element);
-Node *temp = head;
-while (temp->next != NULL)
-{
-temp = temp->next;
+    Node *newNode = new Node(element);
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
     }
-newNode->next = temp->next;
-temp->next = newNode;
+    newNode->next = temp->next;
+    temp->next = newNode;
 }
 void traversal(Node *head)
 {
-Node *temp = head;
-while (temp != NULL)
-{
-cout << temp->data << endl;
-temp = temp->next;
-}
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
 }
 
 int main()
 {
 
-//   Compilation Time code //
-cout << endl;
-auto end = chrono::steady_clock::now();
-auto diff = end - start;
-cout << chrono::duration<double, milli>(diff).count() << " ms" << endl;
-return 0;
+    //   Compilation Time code //
+    cout << endl;
+    auto end = chrono::steady_clock::now();
+    auto diff = end - start;
+    cout << chrono::duration<double, milli>(diff).count() << " ms" << endl;
+    return 0;
 }
