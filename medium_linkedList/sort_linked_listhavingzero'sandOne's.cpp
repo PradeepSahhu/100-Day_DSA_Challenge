5e4cbd74b1dfca913f3ba30839894b48f6c906e2
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-04-04 00:24:11 +0530

    solved the question of sort the linkedlist containing 0's 1's and 2's

--- medium_linkedList/sort_linked_listhavingzero'sandOne's.cpp
+++ medium_linkedList/sort_linked_listhavingzero'sandOne's.cpp
@@ -43,8 +43,67 @@ void traversal(Node *head)
     }
 }
 
+
+
+Node *sortList(Node *head)
+{
+    Node*temp = head;
+    Node* zeroTemp = new Node(-1);
+    Node* oneTemp = new Node(-1);
+    Node* twoTemp = new Node(-1);
+
+    Node*zeroHead = zeroTemp;
+    Node*oneHead = oneTemp;
+    Node*twoHead = twoTemp;
+
+    while(temp!=NULL){
+        if(temp->data==0){
+            zeroTemp->next = temp;
+            zeroTemp = zeroTemp->next;
+        }else if(temp->data ==1){
+            oneTemp->next = temp;
+            oneTemp = oneTemp->next;
+        }else{
+            twoTemp->next = temp;
+            twoTemp=twoTemp->next;
+        }
+        temp = temp->next;
+    }
+    if(zeroTemp){
+        zeroTemp->next = oneHead->next;
+    }
+    if(oneTemp){
+        oneTemp->next = twoHead->next;
+    }
+    twoTemp->next = nullptr;
+
+    return zeroHead->next;
+    
+}
+
 int main()
 {
+
+    Node *head = new Node(1, nullptr);
+    Node *second = new Node(2, head);
+    Node *third = new Node(1, second);
+    Node *fourth = new Node(2, third);
+    Node *fifth = new Node(0, fourth);
+    Node *sixth = new Node(0, fifth);
+    Node *seventh = new Node(0, sixth);
+    Node *last = new Node(1, seventh);
+
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = sixth;
+    sixth->next = seventh;
+    seventh->next = last;
+    last->next = nullptr;
+    Node*newNode = sortList(head);
+    traversal(newNode);
+
 
     //   Compilation Time code //
     cout << endl;
