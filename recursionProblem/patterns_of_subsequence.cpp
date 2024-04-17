e95f22c05b29c63847226ca34252744cba9bfed1
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-04-17 17:25:30 +0530

    soved the patterns of seb sequence

--- recursionProblem/patterns_of_subsequence.cpp
+++ recursionProblem/patterns_of_subsequence.cpp
@@ -5,13 +5,112 @@ using namespace std;
 
 auto start = chrono::steady_clock::now();
 
+
+//! Print all SubSequence.
+
+void printS(int ind, vector<int> ds, int s, int sum, vector<int> arr, int n)
+{
+    if (ind == n)
+    {
+        if (s == sum)
+        {
+            for (auto it : ds)
+                cout << it << " ";
+            cout << endl;
+        }
+        return;
+    }
+
+    ds.push_back(arr[ind]);
+    s += arr[ind];
+
+    printS(ind + 1, ds, s, sum, arr, n);
+
+
+    s -= arr[ind];
+    ds.pop_back();
+
+    printS(ind + 1, ds, s, sum, arr, n);
+}
+
+//! print only one subsequence
+
+bool printSing(int ind, vector<int> ds, int s, int sum, vector<int> arr, int n)
+{
+    if (ind == n)
+    {
+        //condition satisfied
+        if (s == sum)
+        {
+            for (auto it : ds)
+                cout << it << " ";
+            cout << endl;
+            return true;
+        }
+        else return false;
+    }
+
+    ds.push_back(arr[ind]);
+    s += arr[ind];
+
+    if(printSing(ind + 1, ds, s, sum, arr, n)==true){
+        return true;
+    }
+
+
+    s -= arr[ind];
+    ds.pop_back();
+
+    if(printSing(ind + 1, ds, s, sum, arr, n)==true) return true;
+
+    return false;
+}
+
+//! no of subsequences
+
+int printNumber(int ind, vector<int> ds, int s, int sum, vector<int> arr, int n)
+{
+    if (ind == n)
+    {
+        //condition satisfied
+        if (s == sum)
+        {
+            for (auto it : ds)
+            return 1;
+        }
+        else return 0;
+    }
+
+    ds.push_back(arr[ind]);
+    s += arr[ind];
+
+    int l = printNumber(ind + 1, ds, s, sum, arr, n);
+
+    s -= arr[ind];
+    ds.pop_back();
+
+    int r = printNumber(ind + 1, ds, s, sum, arr, n);
+
+    return l+r;
+}
+
+
+
 int main()
 {
 
-//   Compilation Time code //
-cout << endl;
-auto end = chrono::steady_clock::now();
-auto diff = end - start;
-cout << chrono::duration<double, milli>(diff).count() << " ms" << endl;
-return 0;
+    vector<int> nums = {1, 2, 1};
+    int n = 3;
+    int sum = 2;
+
+    vector<int> ds;
+
+    cout<<printNumber(0, ds, 0, sum, nums, n);
+
+    //   Compilation Time code //
+    cout << endl;
+    auto end = chrono::steady_clock::now();
+    auto diff = end - start;
+    cout << chrono::duration<double, milli>(diff).count() << " ms" << endl;
+    return 0;
 }
