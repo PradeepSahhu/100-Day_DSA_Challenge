69f64b3353c1511319863cd414949224cb8619ec
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-04-19 14:05:32 +0530

    Attempt 1 of the combinationSum solution

---
+++ recursionProblem/combinationSum.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <chrono>
+using namespace std;
+auto start = chrono::steady_clock::now();
+
+
+
+void recursionSolution(vector<vector<int>> &ans,vector<int> &ds,int index, int target, vector<int> combination){
+
+
+    if(index==combination.size()){
+        if(target==0){
+            ans.push_back(ds);
+        }
+    }
+
+
+    // pick condition 
+
+    if(target-combination[index]>0){
+        ds.push_back(combination[index]);
+        recursionSolution(ans,ds,index,target-combination[index],combination);
+    }
+
+
+    // non pick condition.
+
+    ds.pop_back();
+    recursionSolution(ans,ds,index+1,target,combination);
+
+
+
+
+
+  
+
+}
+
+
+vector<int> combinationSum(vector<int> &combination, int target){
+
+    vector<int> ds;
+    vector<vector<int>> ans;
+
+    recursionSolution(ans,ds,0,target,combination);
+
+    return ds;
+}
+
+int main()
+{
+
+//   Compilation Time code //
+cout << endl;
+auto end = chrono::steady_clock::now();
+auto diff = end - start;
+cout << chrono::duration<double, milli>(diff).count() << " ms" << endl;
+return 0;
+}
